fbb9740ffb956fef33626f8dcb780c0a7bf17ba4
Adrian <adrian.horstmann.ah@gmail.com>
2016-11-07 19:12:58 -0800

    Updated DataContainer - added HashMaps for all DataTypes and getter/setter - getToday() not implemented yet.

--- app/src/main/java/tempreture/android/csulb/edu/fitbitapp/DataContainer.java
+++ app/src/main/java/tempreture/android/csulb/edu/fitbitapp/DataContainer.java
@@ -2,6 +2,7 @@ package tempreture.android.csulb.edu.fitbitapp;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by Adrian on 04.11.2016.
@@ -17,9 +18,10 @@ public class DataContainer {
     HashMap<String, String> elevation;
     HashMap<String, String> avrgHeartrate; //TODO have to take a look into it
     HashMap<String, String> timeActive;
+    HashMap<String, String> calories;
 
-    public static DataContainer getInstance() {
-        if(dc == null) {
+    public static synchronized DataContainer getInstance() {
+        if (dc == null) {
             dc = new DataContainer();
         }
         return dc;
@@ -27,10 +29,19 @@ public class DataContainer {
 
     private DataContainer() {
         steps = new HashMap<String, String>();
+        distance = new HashMap<String, String>();
+        elevation = new HashMap<String, String>();
+        avrgHeartrate = new HashMap<String, String>();
+        timeActive = new HashMap<String, String>();
+        calories = new HashMap<String, String>();
     }
 
     public String getTodaySteps() {
-        return ""; //TODO get today item should be maybe last
+        Map.Entry newestEntry;
+        for (Map.Entry<String, String> entry : steps.entrySet()) {
+
+        }
+        return "";
     }
 
     public void addSteppsEntry(String date, String value) {
@@ -41,5 +52,43 @@ public class DataContainer {
         return steps;
     }
 
-    //TODO add methods to add and get other datatypes
+    public void addDistanceEntry(String date, String value) {
+        distance.put(date, value);
+    }
+
+    public HashMap<String, String> getDistance() {
+        return distance;
+    }
+
+    public void addElevationsEntry(String date, String value) {
+        elevation.put(date, value);
+    }
+
+    public HashMap<String, String> getElevation() {
+        return elevation;
+    }
+
+    public void addAvrgHeartrateEntry(String date, String value) {
+        avrgHeartrate.put(date, value);
+    }
+
+    public HashMap<String, String> getAvrgHeartrate() {
+        return avrgHeartrate;
+    }
+
+    public void addTimeactiveEntry(String date, String value) {
+        timeActive.put(date, value);
+    }
+
+    public HashMap<String, String> getTimeActive() {
+        return timeActive;
+    }
+
+    public void addCaloriesEntry(String date, String value) {
+        calories.put(date, value);
+    }
+
+    public HashMap<String, String> getCalories() {
+        return calories;
+    }
 }
---
+++ app/src/main/java/tempreture/android/csulb/edu/fitbitapp/DataType.java
@@ -0,0 +1,9 @@
+package tempreture.android.csulb.edu.fitbitapp;
+
+/**
+ * Created by Adrian on 11/7/2016.
+ */
+
+public enum DataType {
+    STEPS, AVRGHEARTRATE, MINHEARTRATE, MAXHEARTRATE, DISTANCE, ELEVATION, TIMEACTIVE, CALORIES;
+}
