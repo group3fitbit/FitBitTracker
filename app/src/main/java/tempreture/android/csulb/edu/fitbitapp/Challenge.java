fac2f572d572603674bf6fffaa8a0b7d714d4716
ibreaker14 <minijordon@gmail.com>
2016-11-10 01:55:48 -0800

    added Challenge and ChallengeContainer
    mutating Challenges will update the Challenge Objects in ChallengeContainer as well

--- app/src/main/java/tempreture/android/csulb/edu/fitbitapp/Challenge.java
+++ app/src/main/java/tempreture/android/csulb/edu/fitbitapp/Challenge.java
@@ -1,4 +1,128 @@
 package tempreture.android.csulb.edu.fitbitapp;
+import java.text.DecimalFormat;
+import java.util.*;
+
+
+public class Challenge {
+    String name;
+    String challengeType;
+    int goal;
+    Date date;
+    double betAmount;
+    ArrayList<Dummy> participants;
+    boolean over;
+
+    public Challenge(){
+        name="Challenge Name";
+        challengeType="?";
+        goal=0;
+        over = false;
+        participants = new ArrayList<Dummy>();
+    }
+
+    public Challenge(String name,String type, int goal, Date date, double betAmount, ArrayList<Dummy> participants){
+        setName(name);
+        setChallengeType(type);
+        setGoal(goal);
+        setDate(date);
+        setBetAmount(betAmount);
+        setParticipants(participants);
+    }
+
+    public void setName(String n){
+        name = n;
+        replaceChallengeInStore();
+    }
+
+    public void setChallengeType(String ct){
+        challengeType = ct;
+        replaceChallengeInStore();
+    }
+    public void setGoal(int g){
+        goal = g;
+        replaceChallengeInStore();
+    }
+    public void setDate(Date d){
+        date = d;
+        replaceChallengeInStore();
+    }
+    public void setParticipants(ArrayList<Dummy> p){
+        participants = p;
+        replaceChallengeInStore();
+    }
+    public void setBetAmount(double bm){
+        betAmount = bm;
+        replaceChallengeInStore();
+    }
+
+    //challenge is over
+    public void closeChallenge(){
+        over = true;
+        replaceChallengeInStore();
+    }
+
+    public void setChallenge(String name,String type, int goal, Date date, double betAmount, ArrayList<Dummy> participants){
+        setName(name);
+        setChallengeType(type);
+        setGoal(goal);
+        setDate(date);
+        setBetAmount(betAmount);
+        setParticipants(participants);
+        replaceChallengeInStore();
+    }
+
+    //returns whether challenge is over or not
+    public boolean isOver(){
+        return over;
+    }
+
+    public String getName(){
+        return name;
+    }
+    public String getChallengeType(){
+        return challengeType;
+    }
+    public Date getDate(){
+        return date;
+    }
+    public int getType(){
+        return goal;
+    }
+    public double getBetAmount(){
+        DecimalFormat df = new DecimalFormat("#.##");
+        return Double.valueOf(df.format(betAmount));
+    }
+    public ArrayList<Dummy> getParticipants(){
+        return participants;
+    }
+
+    public boolean equals(Challenge c){ //assuming there are no repeats of same challenge name
+        if(c.getName() == this.name){
+
+            return true;
+        }
+        return false;
+    }
+
+    public void replaceChallengeInStore(){
+        for(int i = 0; i < ChallengeStore.getInstance().getChallenges().size();i++) {
+            if(ChallengeStore.getInstance().getChallenges().get(i).getClass().getName() == name){
+                ChallengeStore.getInstance().getChallenges().set(i,this.getClass());
+                break;
+            }
+        }
+    }
+}
+
+/* implementataions:
+-> create a new Challenge: Challenge challenge = new Challenge();
+                       or: Challenge challenge = new Challenge(name,type,goal,date,int days,betAmount,ArrayList<Dummy> participants);
+*/
+
+
+//old version just in case
+/*
+package tempreture.android.csulb.edu.fitbitapp;
 
 import java.text.DecimalFormat;
 import java.util.*;
@@ -73,3 +197,4 @@ public class Challenge {
         return participants;
     }
 }
+*/
